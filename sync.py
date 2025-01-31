596b6634657e2e75e90931a08b66112bcf3af649
ATomkar <omkarhundre@arcitech.ai>
2025-01-31 19:55:25 +0530

    add on the xpath

---
+++ sync.py
@@ -0,0 +1,13 @@
+//div[@class='dashborad-Courses-container']//div[5]//div[2]
+
+refresh
+//button[normalize-space()='Refresh Course']
+
+select all
+//input[@id='select-all']
+
+next
+//button[normalize-space()='Next']
+
+start sync
+//button[normalize-space()='Start Sync']
