a21fdfd0f5670ab239dc605a466f4807fecb91ea
ATomkar <omkarhundre@arcitech.ai>
2025-02-04 18:55:00 +0530

    Update conftest.py

--- Prod/conftest.py
+++ Prod/conftest.py
@@ -23,8 +23,8 @@ def Invoke_Browser(request):
     driver.implicitly_wait(50)
     driver.maximize_window()
     time.sleep(1)
-    # driver.set_window_size(1920, 1080)
     request.cls.driver = driver
+    # driver.set_window_size(1920, 1080)
     # driver.refresh()
     yield
     driver.quit()
@@ -59,53 +59,3 @@ def _capture_screenshot(name):
     driver.get_screenshot_as_file(name)
 
 
-# import pytest
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options as ChromeOptions
-# from selenium.webdriver.firefox.options import Options as FirefoxOptions
-# from selenium.webdriver.edge.options import Options as EdgeOptions
-# from selenium.webdriver.chrome.service import Service as ChromeService
-# from selenium.webdriver.firefox.service import Service as FirefoxService
-# from selenium.webdriver.edge.service import Service as EdgeService
-#
-# driver = None
-#
-#
-# @pytest.fixture(scope="class")
-# def Invoke_Browser(request, browser="chrome"):  # Add browser parameter with default value
-#     global driver
-#
-#     if browser.lower() == "chrome":
-#         options = ChromeOptions()
-#         options.add_experimental_option("detach", True)
-#         options.add_argument('ignore-certificate-errors')
-#         options.add_argument("--disable-application-cache")
-#         options.add_argument("--incognito")
-#         service_obj = ChromeService()
-#         driver = webdriver.Chrome(service=service_obj, options=options)
-#
-#     elif browser.lower() == "firefox":
-#         options = FirefoxOptions()
-#         options.add_argument('--ignore-certificate-errors')
-#         options.add_argument('--private')
-#         service_obj = FirefoxService()
-#         driver = webdriver.Firefox(service=service_obj, options=options)
-#
-#     elif browser.lower() == "edge":
-#         options = EdgeOptions()
-#         options.add_argument('--ignore-certificate-errors')
-#         options.add_argument('--inprivate')
-#         service_obj = EdgeService()
-#         driver = webdriver.Edge(service=service_obj, options=options)
-#
-#     else:
-#         raise ValueError(f"Browser {browser} is not supported")
-#
-#     driver.implicitly_wait(50)
-#     driver.get("https://qat.srds.ai/")
-#     driver.maximize_window()
-#     request.cls.driver = driver
-#     driver.refresh()
-#
-#     yield
-#     driver.quit()
